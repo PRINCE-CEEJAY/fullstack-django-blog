0627618d5c2a23d80697883ac89b9edd722dce5b
PRINCE-CEEJAY <ceejaydroidprince@gmail.com>
2026-03-02 22:14:59 -0800

    added POST add_post and view_posts logic

--- posts/urls.py
+++ posts/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('add_post/', views.add_post, name='add_post'),
-    path('view_post/', views.view_posts, name='view_posts'),
+    path('view_posts/', views.view_posts, name='view_posts'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,12 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from .models import POST
 # Create your views here.
+
 @login_required
 def add_post(request):
     if request.method == "POST":
-        pass
-    return render(request, 'posts/add_post.html')
+        data = request.POST
+        title = data.get('title')
+        text = data.get('text')
+
+        if not title or not text:
+            return render(request, 'posts/add_post.html')
+        
+        new_post = POST(title=title.upper(), text=text)
+        new_post.save()
+        return redirect('view_posts')
+    else:            
+        return render(request, 'posts/add_post.html')
 
 @login_required
 def view_posts(request):
-    return render(request, 'posts/view_posts.html')
+    posts = POST.objects.all().order_by('-createdAt') # sort in descending date order
+    return render(request, 'posts/view_posts.html', {'posts': posts})
